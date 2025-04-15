f481e6bfe814330ea37418befa3abcab465e11a3
Rafael Souza <rafasansou1@gmail.com>
2025-04-15 11:31:35 -0300

    adicionando o save dos arquivos

--- projetos_aula01/exemplo_01.py
+++ projetos_aula01/exemplo_01.py
@@ -1,3 +1,4 @@
+#Pedir para o usuário inserir o dado / mostrar da tela um ola e o nome do usuario
 input("Digite seu nome: ")
 
 print("Ola, " + input("Digite seu nome: "))
@@ -5,3 +6,4 @@ print("Ola, " + input("Digite seu nome: "))
 #Criar um programa que o usuario digita seu nome e retorna o numero de caracteres
 
 print(len(input("Digite o seu nome: ")))
+
--- projetos_aula01/exemplo_02.py
+++ projetos_aula01/exemplo_02.py
@@ -1,3 +1,11 @@
 #Criar um programa que o usuario digita 2 valores e apareça a soma
 
 print(int(input("Digite o primeiro numero: ")) + int(input("Digite o segundo numero: ")))
+
+#variaveis
+
+numero = 3 #int
+numero_dec = 104.75 #float
+nome = "Rafael" #string
+verdadeiro = True #booleano
+SENHA_AWS = 198378924612 #maiusculo na variavel representa constante
