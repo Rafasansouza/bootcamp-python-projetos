d49bd14feec10a9e8980ec24b05c00f9c5a421e7
Rafael Souza <rafasansou1@gmail.com>
2025-04-15 15:43:16 -0300

    correção exercicio 14 (string)

--- projetos_aula02/exercicios_string.py
+++ projetos_aula02/exercicios_string.py
@@ -16,9 +16,10 @@ print(frase)
 
 # 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
 
-data = input("Digite uma data no formato dd/mm/aaaa: ").split(sep= "/")
-print(data)
-
+lista_data = input("Digite uma data no formato dd/mm/aaaa: ").split(sep= "/")
+print("O dia é: ", lista_data[0])
+print("O mês é: ", lista_data[1])
+print("O ano é: ", lista_data[2])
 
 # 15. Escreva um programa que concatene duas strings fornecidas pelo usuário.
 
