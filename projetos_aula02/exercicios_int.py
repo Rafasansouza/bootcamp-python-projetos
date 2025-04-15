a2bb1a485d30c0d98a79e91ef2eea23005a87015
Rafael Souza <rafasansou1@gmail.com>
2025-04-15 13:53:03 -0300

    adicionando exercicios de inteiros

---
+++ projetos_aula02/exercicios_int.py
@@ -0,0 +1,33 @@
+# 1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+
+# num_1 = int(input("Digite o primeiro número inteiro: "))
+# num_2 = int(input("Digite o Segundo número inteiro: "))
+# soma = num_1 + num_2
+# print("A soma é:", soma)
+
+# 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+
+# salario = int(input("Digite o salario inteiro: "))
+# DENOMINADOR = int(input("Digite um numero inteiro: "))
+# resto = salario % DENOMINADOR
+# print(f"O resto da divisão de {salario} por {DENOMINADOR} é:", resto)
+
+# 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+
+# num_1 = int(input("Digite o primeiro número inteiro: "))
+# num_2 = int(input("Digite o Segundo número inteiro: "))
+# multiplicacao = num_1 * num_2
+# print("A divisao é:", multiplicacao)
+
+# 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+
+# num_1 = int(input("Digite o primeiro número inteiro: "))
+# num_2 = int(input("Digite o Segundo número inteiro: "))
+# divisao = num_1 / num_2
+# print("A divisao é:", divisao)
+
+# 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
+
+num_1 = int(input("Digite um número inteiro: "))
+raiz = num_1 ** 2
+print("A divisao é:", raiz)
