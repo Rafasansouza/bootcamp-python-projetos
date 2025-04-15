30ac21a1841ed7a861fc4011ead7d5fc6abb9f77
Rafael Souza <rafasansou1@gmail.com>
2025-04-15 11:11:04 -0300

    exemplos aula01

---
+++ projetos_aula01/exemplo_01.py
@@ -0,0 +1,7 @@
+input("Digite seu nome: ")
+
+print("Ola, " + input("Digite seu nome: "))
+
+#Criar um programa que o usuario digita seu nome e retorna o numero de caracteres
+
+print(len(input("Digite o seu nome: ")))
---
+++ projetos_aula01/exemplo_02.py
@@ -0,0 +1,3 @@
+#Criar um programa que o usuario digita 2 valores e apareça a soma
+
+print(int(input("Digite o primeiro numero: ")) + int(input("Digite o segundo numero: ")))
