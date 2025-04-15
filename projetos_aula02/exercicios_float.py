70283caed8c8705c2aeea22c3cfca06a255c1c79
Rafael Souza <rafasansou1@gmail.com>
2025-04-15 14:43:54 -0300

    adicionando exercicios float 2

--- projetos_aula02/exercicios_float.py
+++ projetos_aula02/exercicios_float.py
@@ -1,30 +1,30 @@
 # 6. Escreva um programa que receba dois números flutuantes e realize sua adição.
 
-# num_1 = float(input("Digite o primeiro número decimal: "))
-# num_2 = float(input("Digite o segundo número decimal: "))
-# soma = num_1 + num_2
-# print("A soma é:", soma)
+num_1 = float(input("Digite o primeiro número decimal: "))
+num_2 = float(input("Digite o segundo número decimal: "))
+soma = num_1 + num_2
+print("A soma é:", soma)
 
 # # 7. Crie um programa que calcule a média de dois números flutuantes fornecidos pelo usuário.
 
-# num_1 = float(input("Digite o primeiro número decimal: "))
-# num_2 = float(input("Digite o segundo número decimal: "))
-# media = (num_1 + num_2) / 2
-# print("A soma é:", media)
+num_1 = float(input("Digite o primeiro número decimal: "))
+num_2 = float(input("Digite o segundo número decimal: "))
+media = (num_1 + num_2) / 2
+print("A soma é:", media)
 
 # 8. Desenvolva um programa que calcule a potência de um número (base e expoente fornecidos pelo usuário).
 
-# base = float(input("Digite a base em número decimal: "))
-# exp = float(input("Digite o expoente em número decimal: "))
-# potenc = base ** exp
-# print("A soma é:", potenc)
+base = float(input("Digite a base em número decimal: "))
+exp = float(input("Digite o expoente em número decimal: "))
+potenc = base ** exp
+print("A soma é:", potenc)
 
 
 # 9. Faça um programa que converta a temperatura de Celsius para Fahrenheit.
 
-# temp_C = float(input("Digite a temperatura em °C: "))
-# convert_to_F = (temp_C * (9/5)) + 32
-# print(f"{temp_C} °C equivalem {convert_to_F} °F.")
+temp_C = float(input("Digite a temperatura em °C: "))
+convert_to_F = (temp_C * (9/5)) + 32
+print(f"{temp_C} °C equivalem {convert_to_F} °F.")
 
 # 10. Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
 
