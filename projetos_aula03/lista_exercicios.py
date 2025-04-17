1f065430dfbe49e37e4e02d860af94e144132357
Rafael Souza <rafasansou1@gmail.com>
2025-04-17 20:35:30 -0300

    adicionando exercicio 2

--- projetos_aula03/lista_exercicios.py
+++ projetos_aula03/lista_exercicios.py
@@ -3,20 +3,34 @@
 # que todos os registros tenham valores positivos para `quantidade` e `preço`. 
 # Escreva um programa que verifique esses campos e imprima "Dados válidos" se ambos 
 # forem positivos ou "Dados inválidos" caso contrário.
-try:
-    quantidade = int(input("Digite a quantidade vendida (inteiro): "))
-    preco = float(input("Digite o preço (Decimal): "))
-    if quantidade > 0 and preco > 0:
-        print("Dados válidos!")
-    else:
-        print("Dados inválidos!")
-except ValueError:
-    print("Por favor, digite números válidos.")
+# try:
+#     quantidade = int(input("Digite a quantidade vendida (inteiro): "))
+#     preco = float(input("Digite o preço (Decimal): "))
+#     if quantidade > 0 and preco > 0:
+#         print("Dados válidos!")
+#     else:
+#         print("Dados inválidos!")
+# except ValueError:
+#     print("Por favor, digite números válidos.")
 
 ### Exercício 2: Classificação de Dados de Sensor
 # Imagine que você está trabalhando com dados de sensores IoT. 
 # Os dados incluem medições de temperatura. Você precisa classificar cada leitura 
 # como 'Baixa', 'Normal' ou 'Alta'. Considerando que:
+# Temperatura < 18°C é 'Baixa'
+# Temperatura >= 18°C e <= 26°C é 'Normal'
+# Temperatura > 26°C é 'Alta'
+
+try:
+    temperatura = float(input("Digite a temperatura: "))
+    if temperatura < 18:
+        print("Baixa")
+    elif temperatura > 26:
+        print("Alta")
+    else:
+        print("Normal")
+except ValueError:
+    print("Erro: Esse tipo de temperatura não existe!")
 
 ### Exercício 3: Filtragem de Logs por Severidade
 # Você está analisando logs de uma aplicação e precisa filtrar mensagens 
