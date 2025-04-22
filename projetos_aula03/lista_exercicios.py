cf7cad5b91badd86e0d36754d09d0c5004e550c4
Rafael Souza <rafasansou1@gmail.com>
2025-04-22 15:49:16 -0300

    corrigindo resolução 15

--- projetos_aula03/lista_exercicios.py
+++ projetos_aula03/lista_exercicios.py
@@ -186,7 +186,7 @@ print("Todas as páginas foram processadas.")
 ### Exercício 15. Processamento de Dados com Condição de Parada
 # Processar itens de uma lista até encontrar um valor específico que indica a parada.
 
-tens = [1, 2, 3, "parar", 4, 5]
+itens = [1, 2, 3, "parar", 4, 5]
 
 i = 0
 while i < len(itens):
