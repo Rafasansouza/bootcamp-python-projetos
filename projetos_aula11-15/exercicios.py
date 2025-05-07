3c582ab732fe763820dddb61d4e43f8e67a63b6b
Rafael Souza <rafasansou1@gmail.com>
2025-05-07 20:46:19 -0300

    adicionando exercicio 1

---
+++ projetos_aula11-15/exercicios.py
@@ -0,0 +1,10 @@
+import math
+
+# #### Inteiros (`int`)
+
+# 1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+
+numero_01 = int(input("Inserir um numero inteiro: "))
+numero_02 = int(input("Inserir outro numero inteiro: "))
+resultado = numero_01 + numero_02
+print(resultado)
