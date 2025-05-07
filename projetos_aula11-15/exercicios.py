f5dc555ec0dc8540edf3b8738c860434a6c9db71
Rafael Souza <rafasansou1@gmail.com>
2025-05-07 20:48:25 -0300

    adicionando exercicio 3

--- projetos_aula11-15/exercicios.py
+++ projetos_aula11-15/exercicios.py
@@ -15,3 +15,10 @@ numero_01 = int(input("Inserir um numero inteiro: "))
 resultado = numero_01 % 5
 print(resultado)
 
+# 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+
+numero_01 = int(input("Inserir um numero inteiro: "))
+numero_02 = int(input("Inserir outro numero inteiro: "))
+resultado = numero_01 * numero_02
+print(resultado)
+
